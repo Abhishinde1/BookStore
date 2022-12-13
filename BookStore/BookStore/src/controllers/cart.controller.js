0110ef7ec011bafae96e3da3e2fd67177559a46e
AbhiShinde1 <abhishinde572@gmail.com>
2022-12-13 18:16:11 +0530

    [Abhishek]Added:remove book from cart

--- BookStore/BookStore/src/controllers/cart.controller.js
+++ BookStore/BookStore/src/controllers/cart.controller.js
@@ -17,3 +17,42 @@ export const addedToCart = async (req, res, next) => {
         next(error);
     }
 };
+
+
+//Controller to Remove a book  Quantity from cart
+export const removeBook = async (req, res, next) => {
+    try {
+        const data = await cartService.removeBook(req.body.EmailId, req.params._id);
+        res.status(HttpStatus.CREATED).json({
+            code: HttpStatus.CREATED,
+            data: data,
+            message: 'Book removed successfully'
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+
+/**
+ * Controller to remove books from cart
+ * @param  {object} req - request object
+ * @param {object} res - response object
+ * @param {Function} next
+ */
+export const removeBookFromCart = async (req, res) => {
+  try {
+    const data = await cartService.removeBookFromCart(req.body, req.params._id);
+    res.status(HttpStatus.OK).json({
+      code: HttpStatus.OK,
+      data: data,
+      message: 'book removed from cart successfully'
+    });
+  } catch (error) {
+    res.status(HttpStatus.BAD_REQUEST).json({
+      code: HttpStatus.BAD_REQUEST,
+      message: `${error}`
+    });
+  }
+};
+
--- BookStore/BookStore/src/routes/cart.routes.js
+++ BookStore/BookStore/src/routes/cart.routes.js
@@ -9,5 +9,10 @@ const router = express.Router();
 //router to add books to the cart
 router.post('/:_id', userAuth, cartController.addedToCart);
 
+//route to remove book Quantity from the cart
+router.put('/remove/:_id', userAuth, cartController.removeBook);
+
+//route to remove books from the cart
+router.put('/:_id/', userAuth, cartController.removeBookFromCart);
 
 export default router;

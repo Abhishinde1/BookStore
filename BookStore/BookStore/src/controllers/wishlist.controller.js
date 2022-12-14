15e9a2ed8fd13121a0385030e4667cbac94eee43
AbhiShinde1 <abhishinde572@gmail.com>
2022-12-14 15:06:22 +0530

    [Abhishek]Added: Added to Wishlist

---
+++ BookStore/BookStore/src/controllers/wishlist.controller.js
@@ -0,0 +1,24 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable max-len */
+/* eslint-disable prettier/prettier */
+import HttpStatus from 'http-status-codes';
+import * as wishlistService from '../services/wishlist.service';
+
+
+export const addBookToWishlist = async (req, res) => {
+    try {
+        const data = await wishlistService.addBookToWishlist(req.body.EmailId, req.params._id);
+        res.status(HttpStatus.CREATED).json({
+            code: HttpStatus.CREATED,
+            data: data,
+            message: 'Added to Wishlist successfully'
+        });
+    } catch (error) {
+        res.status(HttpStatus.BAD_REQUEST).json({
+            code: HttpStatus.BAD_REQUEST,
+            message: `${error}`
+        })
+    }
+};
+
+
---
+++ BookStore/BookStore/src/routes/wishlist.routes.js
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import express from 'express';
+import * as wishlistcontroller from '../controllers/wishlist.controller';
+import { userAuth } from '../middlewares/auth.middleware';
+
+
+const router = express.Router();
+
+//router to add books to the cart
+router.post('/:_id', userAuth, wishlistcontroller.addBookToWishlist);
+
+export default router;
